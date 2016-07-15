15b81f3fe9bfa4e72ccef2d5e2346ab00943b5a3
Jordan Smith <bear.chunda@gmail.com>
2016-07-15 12:12:46 -0500

    feat(ref): skill checks data omg :fu: so much

--- app/routes/reference/skills.js
+++ app/routes/reference/skills.js
@@ -1,4 +1,118 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      acrobatics: acrobatics,
+      bluff: bluff,
+      climb: climb,
+      diplomacy: diplomacy,
+      disableDevice: disableDevice,
+      disguise: disguise,
+      escapeArtist: escapeArtist,
+      fly: fly,
+      handleAnimals: handleAnimals,
+      heal: heal,
+      knowledge: knowledge,
+      linguistics: linguistics,
+      perception: perception,
+      ride: ride,
+      spellcraft: spellcraft,
+      survival: survival,
+      swim: swim,
+      survival: survival,
+      useMagicDevice: useMagicDevice
+    });
+  }
 });
+
+let acrobatics = [
+  {
+    name: "City of Fire",
+  }
+];
+let bluff = [
+  {
+    name: "City of Fire",
+  }
+];
+let climb = [
+  {
+    name: "City of Fire",
+  }
+];
+let diplomacy = [
+  {
+    name: "City of Fire",
+  }
+];
+let disableDevice = [
+  {
+    name: "City of Fire",
+  }
+];
+let disguise = [
+  {
+    name: "City of Fire",
+  }
+];
+let escapeArtist = [
+  {
+    name: "City of Fire",
+  }
+];
+let fly = [
+  {
+    name: "City of Fire",
+  }
+];
+let handleAnimals = [
+  {
+    name: "City of Fire",
+  }
+];
+let heal = [
+  {
+    knowledge: "City of Fire",
+  }
+];
+let knowledge = [
+  {
+    name: "City of Fire",
+  }
+];
+let linguistics = [
+  {
+    name: "City of Fire",
+  }
+];
+let perception = [
+  {
+    name: "City of Fire",
+  }
+];
+let ride = [
+  {
+    name: "City of Fire",
+  }
+];
+let spellcraft = [
+  {
+    name: "City of Fire",
+  }
+];
+let survival = [
+  {
+    name: "City of Fire",
+  }
+];
+let swim = [
+  {
+    name: "City of Fire",
+  }
+];
+let useMagicDevice = [
+  {
+    name: "City of Fire",
+  }
+];
